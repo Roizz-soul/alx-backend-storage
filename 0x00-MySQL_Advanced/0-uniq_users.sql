3ca3904c251dd78f8be0658e01b3c5bddc3bd8ed
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-08-22 16:09:58 +0000

    Task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- SQL script to create a table with tight requirements
+-- Create table
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,2 @@
+# Advanced MySQL
+Here I'll  be learning how to use indexes, stored procedures, functions, triggers and views in MySQL
