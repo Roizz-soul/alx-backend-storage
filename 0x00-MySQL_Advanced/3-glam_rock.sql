1321ecc2892f0d51e07c7d0040aca623152f055d
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-08-23 13:39:21 +0000

    Task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,13 +1,7 @@
 -- Script to list stuff
 -- SQL script to list Glam rock bands ranked by their longevity
-
 -- Select band_name and lifespan from metal band table
-SELECT 
-    band_name,
-    (IFNULL(split, '2020') - formed) AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-ORDER BY 
-    lifespan DESC;
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
